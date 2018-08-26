7b1afc49f8e39237e740076ebce4a7ae5fa96dc9
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2018-08-26 16:28:41 -0500

    added example config

---
+++ modtools/examples/config.py
@@ -0,0 +1,25 @@
+# discord auth
+discordtoken = ''
+# reddit auth
+redditusername = 's4njee'
+redditpassword = ''
+client_secret = ''
+client_id = ''
+
+#mod emojis - use reddit username and emoji or unicode string
+modemojis = {'edgykitty':'🐱','Uncle_Freddy':u"\U0001F1F5",'IamOlderthanMe':'🐝','MonsterDevourer':'😂', 'FriendEater':'🤰', 's4njee': '🤘', 'AutoModerator': '🤖', 'DoesNotChodeWell': u"\U0001F1E8",
+             'iamtheraptor': '🦌', 'widesheep': u"\U0001F411", 'FeversMirrors': u"\U0001F340",
+             'DrTom': '🌀', 'nick168':'🐮', 'RonFucking_Swanson':'🇮🇹', 'brexbre': u"\U0001F1E7",
+             'RedditIsTheDevil': u"\U0001F1F5", "BabyS1othWithA1": u"\U0001F1F2", 'sptagnew':u"\U0001F1F0",
+             'LieutenantKumar':'🐦', 'Chancelor_West':'😈', 'havfunonline':'🍩', 'rps215':'🐎', 'airtmac24':u"\U0001F1E6"}
+
+#for tagging when a user is mentioned in modmail, use reddit username
+discordIDs = {'Chancelor_West':'145661914016514048', 'BabyS1othWithA1':'279621759500550144',
+              'LieutenantKumar':'279613366295592961', 's4njee' : '289427343884288000'}
+
+#channel this feeds to 
+channel = '475157947144601608'
+#channel for modmails, can be same as above
+modmailchannel = '476094282705403916'
+#your subreddit
+subreddit = 'nba'
